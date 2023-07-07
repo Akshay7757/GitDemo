9e32fe5a27ad06a903bbac52302d22d8b7a02fe8
Bitinject77 <131004625+Bitinject77@users.noreply.github.com>
2023-07-07 10:27:47 +0530

    Update MyFirst.java
    Third Case Updated

--- GitDemo/src/com/test/MyFirst.java
+++ GitDemo/src/com/test/MyFirst.java
@@ -5,6 +5,7 @@ public class MyFirst {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java");
 		System.out.println("This is Second case");
+		System.out.println("This is Third case");
 
 	}
 
